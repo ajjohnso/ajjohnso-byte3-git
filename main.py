eace178de690e0528ea7803490d7a472a6429d60
Amanda Johnson <amandajohnson1023@gmail.com>
2017-01-26 20:39:52 -0500

    add SQL

--- main.py
+++ main.py
@@ -16,3 +16,59 @@ def hello():
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+
+import os
+
+import MySQLdb
+import webapp2
+
+
+# These environment variables are configured in app.yaml.
+CLOUDSQL_CONNECTION_NAME = os.environ.get('ajjohnso-aware')
+CLOUDSQL_USER = os.environ.get('amandajohnso1023')
+CLOUDSQL_PASSWORD = os.environ.get('Oink0ink')
+
+
+def connect_to_cloudsql():
+    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
+    # will be set to 'Google App Engine/version'.
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        # Connect using the unix socket located at
+        # /cloudsql/cloudsql-connection-name.
+        cloudsql_unix_socket = os.path.join(
+            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+
+        db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            passwd=CLOUDSQL_PASSWORD)
+
+    # If the unix socket is unavailable, then try to connect using TCP. This
+    # will work if you're running a local MySQL server or using the Cloud SQL
+    # proxy, for example:
+    #
+    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+    #
+    else:
+        db = MySQLdb.connect(
+            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+
+    return db
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        """Simple request handler that shows all of the MySQL variables."""
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        db = connect_to_cloudsql()
+        cursor = db.cursor()
+        cursor.execute('SHOW VARIABLES')
+
+        for r in cursor.fetchall():
+            self.response.write('{}\n'.format(r))
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
